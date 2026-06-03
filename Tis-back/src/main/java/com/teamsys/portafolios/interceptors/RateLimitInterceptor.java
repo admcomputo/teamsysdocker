d7bf0056927ee46dd95b2fb53ebbe06278f73937
jose quiroz perez <201904925@est.umss.edu>
2026-06-03 14:54:41 -0400

    fix: permitir options

--- Tis-back/src/main/java/com/teamsys/portafolios/interceptors/RateLimitInterceptor.java
+++ Tis-back/src/main/java/com/teamsys/portafolios/interceptors/RateLimitInterceptor.java
@@ -18,7 +18,7 @@ public class RateLimitInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-        // IMPORTANTE: permitir solicitudes OPTIONS para evitar bloqueo CORS
+        // MUY IMPORTANTE PARA CORS
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             return true;
         }
