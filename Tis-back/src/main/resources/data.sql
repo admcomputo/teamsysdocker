0a05fafddcdcb937f24b673752a43e05f50c0667
Jose Quiroz Perez <201904925@est.umss.edu>
2026-06-24 01:35:05 -0400

    f

--- Tis-back/src/main/resources/data.sql
+++ Tis-back/src/main/resources/data.sql
@@ -1,7 +1,55 @@
-INSERT INTO roles (id, nombre)
-VALUES (1, 'ROLE_USER')
-ON DUPLICATE KEY UPDATE nombre = VALUES(nombre);
+INSERT INTO roles (nombre_rol)
+VALUES ('ROLE_USER')
+ON DUPLICATE KEY UPDATE nombre_rol = VALUES(nombre_rol);
 
-INSERT INTO roles (id, nombre)
-VALUES (2, 'ROLE_ADMIN')
-ON DUPLICATE KEY UPDATE nombre = VALUES(nombre);
+INSERT INTO roles (nombre_rol)
+VALUES ('ROLE_ADMIN')
+ON DUPLICATE KEY UPDATE nombre_rol = VALUES(nombre_rol);
+
+INSERT INTO profesiones (nombre_profesion)
+VALUES ('Ingeniería de Sistemas')
+ON DUPLICATE KEY UPDATE nombre_profesion = VALUES(nombre_profesion);
+
+INSERT INTO profesiones (nombre_profesion)
+VALUES ('Diseño Gráfico')
+ON DUPLICATE KEY UPDATE nombre_profesion = VALUES(nombre_profesion);
+
+INSERT INTO profesiones (nombre_profesion)
+VALUES ('Marketing Digital')
+ON DUPLICATE KEY UPDATE nombre_profesion = VALUES(nombre_profesion);
+
+INSERT INTO categorias (nombre, clasificacion)
+VALUES ('Frontend', 'TECNICA')
+ON DUPLICATE KEY UPDATE clasificacion = VALUES(clasificacion);
+
+INSERT INTO categorias (nombre, clasificacion)
+VALUES ('Backend', 'TECNICA')
+ON DUPLICATE KEY UPDATE clasificacion = VALUES(clasificacion);
+
+INSERT INTO categorias (nombre, clasificacion)
+VALUES ('Base de Datos', 'TECNICA')
+ON DUPLICATE KEY UPDATE clasificacion = VALUES(clasificacion);
+
+INSERT INTO categorias (nombre, clasificacion)
+VALUES ('Comunicación', 'BLANDA')
+ON DUPLICATE KEY UPDATE clasificacion = VALUES(clasificacion);
+
+INSERT INTO categorias (nombre, clasificacion)
+VALUES ('Trabajo en equipo', 'BLANDA')
+ON DUPLICATE KEY UPDATE clasificacion = VALUES(clasificacion);
+
+INSERT INTO tecnologias (nombre, categoria, logo_url)
+VALUES ('React', 'Frontend', '')
+ON DUPLICATE KEY UPDATE categoria = VALUES(categoria), logo_url = VALUES(logo_url);
+
+INSERT INTO tecnologias (nombre, categoria, logo_url)
+VALUES ('Java', 'Backend', '')
+ON DUPLICATE KEY UPDATE categoria = VALUES(categoria), logo_url = VALUES(logo_url);
+
+INSERT INTO tecnologias (nombre, categoria, logo_url)
+VALUES ('Spring Boot', 'Backend', '')
+ON DUPLICATE KEY UPDATE categoria = VALUES(categoria), logo_url = VALUES(logo_url);
+
+INSERT INTO tecnologias (nombre, categoria, logo_url)
+VALUES ('MySQL', 'Base de Datos', '')
+ON DUPLICATE KEY UPDATE categoria = VALUES(categoria), logo_url = VALUES(logo_url);
